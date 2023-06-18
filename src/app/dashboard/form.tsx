bcad78c3a42f109dad6e51bba73a058a77a0cd4b
DeltaEcho192 <anthony@durrer.co.za>
2023-06-18 15:07:23 +0000

    Added route search and response for a test input

--- src/app/dashboard/form.tsx
+++ src/app/dashboard/form.tsx
@@ -7,6 +7,33 @@ export default function LocationForm() {
 	const [waypoints, setWaypoints] = useState<any[]>([]);
 	var nextId = 0;
 
+	const getRoute = async () => {
+			const data = {
+				route: waypoints
+			}
+			const JSONdata = JSON.stringify(data)
+
+			const endpoint = 'http://localhost:3100/api/route'
+			console.log(JSONdata);
+			const options = {
+				// The method is POST because we are sending data.
+				method: 'POST',
+				// Tell the server we're sending JSON.
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Headers': '*'
+				},
+				// Body of the request is the JSON data we created above.
+				body: JSONdata,
+			}
+			const response = await fetch(endpoint, options)
+
+			const result = await response.json()
+			console.log(result)
+			alert(`First waypoint is ${result.firstWaypoint}`)
+	}
+
 	return (
 			<div>
 			<label htmlFor="waypoint"> Waypoint Entry </label>
@@ -20,14 +47,13 @@ export default function LocationForm() {
 
 
 
-			<h1> Waypoints </h1>
+			<h3> Waypoints </h3>
 			<ul>
 			{waypoints.map(waypoint =>(
 						<li key={waypoint.id}>{waypoint.location}</li>
 						))}
 			</ul>
+			<button onClick={getRoute}>Get Route </button>
 			</div>
 	)
 }
-
-
